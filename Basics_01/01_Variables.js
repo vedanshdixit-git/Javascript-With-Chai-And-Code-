123524fcdad5f81b5bdd66d413895e7da9c162ec
Vedansh Dixit <dixitvedansh05@gmail.com>
2025-10-12 07:52:49 +0000

    Datatypes And Variables

---
+++ Basics_01/01_Variables.js
@@ -0,0 +1,15 @@
+const accountId = 11446561
+let accountEmail ="dixitvedansh@gmail.com"
+var accountPassword = "12498"
+accountCity = "Lucknow"
+
+// accountId = 5 //Not allowed 
+// console.log(accountId)
+// accountEmail ="Vedansh"
+// console.log(accountEmail);
+// accountPassword = 154659
+// console.log(accountPassword);
+console.table([accountEmail,accountId,accountPassword,accountCity])
+// Prefer Not to use Var 
+let accountstate;
+console.log(accountstate)
---
+++ Basics_01/02_Datatypes.js
@@ -0,0 +1,20 @@
+"Use strict"//Uses JS newer version
+
+let Name = "Vedansh"
+let age = 19
+let isLoggedIn=false
+let state;
+
+/* number=>2 to power 53
+bigint
+string=> "Whatever we type here is string"
+boolean => true / false
+null
+undefined
+symbol
+
+
+
+Object
+*/
+console.log(typeof null);
