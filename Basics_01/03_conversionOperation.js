83c3a433bfb6b584c60d71de250e31107b258252
Vedansh Dixit <dixitvedansh05@gmail.com>
2025-10-13 07:33:49 +0000

    Conversion of datatypes

---
+++ Basics_01/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let score = "33"
+
+let scoreInNumber=Number(score)
+
+console.log(typeof scoreInNumber);
+
+let scoreInBoolean = Boolean(score)
+console.log(scoreInBoolean);
+
+// 1 - true  0-false
+// "" is false
+// "Vedansh" is true 
+
+let anyNumber= 45
+
+let stringNumber = String(anyNumber)
+console.log(typeof stringNumber);
