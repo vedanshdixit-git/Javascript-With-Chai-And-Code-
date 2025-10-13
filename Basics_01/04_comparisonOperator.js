2da594afa02c81388cf09a5a6c6db48f7a5a62f8
Vedansh Dixit <dixitvedansh05@gmail.com>
2025-10-13 10:16:44 +0000

    Comparison

---
+++ Basics_01/04_comparisonOperator.js
@@ -0,0 +1,19 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2<1);
+// console.log(2!=1);
+// console.log(2==1);
+
+// console.log("2">=1);
+// console.log("02">=1);
+
+
+// Should Avoid these type of comparisons 
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+// === checks values as well as datatype 
+console.log("2"===2);
