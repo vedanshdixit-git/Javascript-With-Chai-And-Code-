909dcf70b57e1e052d481464c5e57195904dc9cf
Vedansh Dixit <dixitvedansh05@gmail.com>
2025-10-13 10:07:34 +0000

    Conversion Complete

--- Basics_01/03_conversionOperation.js
+++ Basics_01/03_conversionOperation.js
@@ -1,3 +1,5 @@
+/*
+
 let score = "33"
 
 let scoreInNumber=Number(score)
@@ -15,3 +17,39 @@ let anyNumber= 45
 
 let stringNumber = String(anyNumber)
 console.log(typeof stringNumber);
+
+*/
+
+// Operations
+
+// let value = 5
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2-2);
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**12);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Vedansh"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+
+console.log(1+"2"+2);
+console.log(1+2+"2");
+console.log(true);
+console.log(+"");
+let num1,num2,num3
+num1=num2=num3= 2+2
+
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter);
