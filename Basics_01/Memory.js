32a98b571f5e5ae5c1dfbb312bdae048d39414ac
Vedansh Dixit <dixitvedansh05@gmail.com>
2025-10-14 16:16:11 +0000

    Stack and Heap data types

---
+++ Basics_01/Memory.js
@@ -0,0 +1,20 @@
+// Stack (Primitive),Heap (Non Primitive)
+
+
+let myYoutubeName="Vedansh"
+anotherName="Full Prank"
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
+let userOne={
+   email:   "dixitvedansh.email",
+   upi: "Vedansh@pingpay" 
+}
+
+let userTwo=userOne
+userTwo.email="Vedansh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
