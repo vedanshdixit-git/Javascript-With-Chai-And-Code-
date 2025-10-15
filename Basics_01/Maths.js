511130407498b069ca9a43ae5e7436b08ac38156
Vedansh Dixit <dixitvedansh05@gmail.com>
2025-10-15 16:12:59 +0000

    Math

---
+++ Basics_01/Maths.js
@@ -0,0 +1,18 @@
+console.log(Math);
+/*
+console.log(Math.abs(-4));//absolute 
+console.log(Math.round(4.333));//round off figures
+console.log(Math.ceil(5.5652));
+console.log(Math.floor(9.22));
+console.log(Math.min(2, 6, 8, 5, 10));
+console.log(Math.max(2, 6, 8, 5, 10));
+
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+
+*/
+const min = 15
+const max = 30
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
