e9770870688df9c9f6fa632998b3ce564df99728
Vedansh Dixit <dixitvedansh05@gmail.com>
2025-10-16 14:53:19 +0000

    Dates Basics

---
+++ Basics_01/Dates_basics.js
@@ -0,0 +1,19 @@
+Date 
+console.log(Date);
+let myDate = new Date()
+console.log(typeof(myDate));
+
+
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toUTCString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+
